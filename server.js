98835e606dcc4d2a66244d81ec49b035205dcda1
nise81 <niels.seidel@nise81.com>
2015-11-06 00:24:45 +0100

    updated data

--- server.js
+++ server.js
@@ -125,7 +125,7 @@ var conn = mongoose.connect( 'mongodb://localhost/' + application , function(err
 		}
 		
 		if( application === 'etutor' ){	
-			videos.csvImport(); // !!! caution
+			//videos.csvImport(); // !!! caution
 			users.csvImport();
 			scripts.importScript();
 			groups.csvImport();
